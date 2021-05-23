ba3c2c5853ed07369518604670ca312892b27773
Smirnova Anastasia <thorloky007@gmail.com>
2021-05-23 23:47:40 +0500

    Add files via upload

---
+++ DeleteObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompGraf1
+{
+    public class DeleteObject
+    {
+        public NewObject ReturnShouldBeRemove(LinkedList<NewObject> stackObjects, NewObject shouldBeRemove, Graphics g, Label label_message, string CURRENT_OBJECT)
+        {
+                if (stackObjects.Count() != 0 && stackObjects.Find(shouldBeRemove) != null) { stackObjects.Remove(shouldBeRemove); }
+                g.Clear(Color.White);
+                label_message.Text = "";
+                if (stackObjects.Count != 0)
+                {
+                    foreach (NewObject l in stackObjects.Reverse())
+                    {
+                        switch (l.OBJECT_NAME)
+                        {
+                            case "LINE":
+                                l.DrawNewLine(g);
+                                if (CURRENT_OBJECT == "LINE")
+                                    shouldBeRemove = l;
+                                break;
+                            case "CIRCLE":
+                                if (CURRENT_OBJECT == "CIRCLE")
+                                    shouldBeRemove = l;
+                                l.FillNewCircle(g);
+                                break;
+                            case "POLYGON":
+                                if (CURRENT_OBJECT == "POLYGON")
+                                    shouldBeRemove = l;
+                                l.FillNewPolygon(g);
+                                break;
+                            case "R_POLYGON":
+                                if (CURRENT_OBJECT == "R_POLYGON")
+                                    shouldBeRemove = l;
+                                l.FillNewRPolygon(g);
+                                break;
+                        }
+                    }
+                }
+                return shouldBeRemove;
+            }
+        
+    }
+}
--- NewObject.cs
+++ NewObject.cs
@@ -1,35 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CompGraf2
+namespace CompGraf1
 {
-    class NewObject
+    public class NewObject
     {
-        public int width;
-        public int height;
+        Point point1;
+        Point point2;
 
-        public Point[] points = new Point[4];
+        int size;
+        public string OBJECT_NAME;
 
-        public NewObject(int width, int height)
+        Point[] points;
+
+        public NewObject() { }
+        public NewObject(TextBox textBox_X1Line, TextBox textBox_Y1Line,
+            TextBox textBox_X2Line, TextBox textBox_Y2Line, TextBox textBox_WLine)
         {
-            this.width = width;
-            this.height = height;
+            //line
+            OBJECT_NAME = "LINE";
+
+            point1 = new Point(int.Parse(textBox_X1Line.Text), int.Parse(textBox_Y1Line.Text));
+            point2 = new Point(int.Parse(textBox_X2Line.Text), int.Parse(textBox_Y2Line.Text));
+            size = int.Parse(textBox_WLine.Text);
         }
 
-        public void StartPoints(Panel panel1, NewObject rec)
+        public NewObject(TextBox textBox_XCenterCircle, TextBox textBox_YCenterCircle,
+            TextBox textBox_radiusCircle)
         {
-            rec.points[0] = new Point(panel1.Width / 2 - rec.width / 2, panel1.Height / 2 - rec.height / 2);
-            rec.points[1] = new Point(panel1.Width / 2 - rec.width / 2 + rec.width, panel1.Height / 2 - rec.height / 2);
+            //circle
+            OBJECT_NAME = "CIRCLE";
 
-            rec.points[2] = new Point(panel1.Width / 2 - rec.width / 2 + rec.width, panel1.Height / 2 - rec.height / 2 + rec.height);
-            rec.points[3] = new Point(panel1.Width / 2 - rec.width / 2, panel1.Height / 2 - rec.height / 2 + rec.height);
+            size = int.Parse(textBox_radiusCircle.Text);
+            point1 = new Point(int.Parse(textBox_XCenterCircle.Text), int.Parse(textBox_YCenterCircle.Text));
 
+        }
+
+        public NewObject(Point[] points)
+        {
+            //polygon
+            OBJECT_NAME = "POLYGON";
+            this.points = points;
+        }
+
+        public NewObject(TextBox textBox_XRPolygon, TextBox textBox_YRPolygon,
+            TextBox textbox_radiusPolygon, TextBox textBox_polygonAngles)
+        {
+            //polygon
+            OBJECT_NAME = "R_POLYGON";
+
+            int angles = int.Parse(textBox_polygonAngles.Text);
+            double alpha = 2 * Math.PI / angles;
+            size = int.Parse(textbox_radiusPolygon.Text);
+
+            point1 = new Point(int.Parse(textBox_XRPolygon.Text), int.Parse(textBox_YRPolygon.Text));
+
+            points = new Point[angles];
+
+            for (int i = 0; i < angles; i++)
+            {
+                int delX = (int)(size * Math.Sin(alpha * i));
+                int delY = (int)(size * Math.Cos(alpha * i));
+                points[i] = new Point(point1.X + delX, point1.Y + delY);
+            }
+        }
+
+        public void DrawNewLine(Graphics g)
+        {
+            Pen p = new Pen(Color.Black, size);
+            g.DrawLine(p, point1, point2);
+        }
+
+        public void FillNewCircle(Graphics g)
+        {
+            SolidBrush b = new SolidBrush(Color.Red);
+            g.FillEllipse(b, point1.X - size, point1.Y - size, size * 2, size * 2);
+        }
+
+        public void FillNewPolygon(Graphics g)
+        {
+            SolidBrush b = new SolidBrush(Color.Blue);
+            g.FillPolygon(b, points);
+        }
+
+        public void FillNewRPolygon(Graphics g)
+        {
+            SolidBrush b = new SolidBrush(Color.DarkGreen);
+            g.FillPolygon(b, points);
         }
     }
-
 }
